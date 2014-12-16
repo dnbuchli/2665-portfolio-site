f6b5687f197b55c22281033eb4878e26395512ca
dnbuchli <db513163@e224l54.southeast.edu>
2014-12-16 14:22:11 -0600

    Updated Contact Form
    Implemented new contact form with php and fixed styling of the submit
    button.

---
+++ contact-thanks.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>editionh | Contact</title>
+	
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" type="text/css" href="css/custom.css"/>
+    <link rel="stylesheet" type="text/css" href="css/fonts/stylesheet.css"/>
+	<link rel="shortcut icon" type="image/png" href="img/logo-tab.png">
+</head>
+
+<body>
+
+<div class="container">
+
+ 	<div class="row">
+      <div class="large-2 medium-2 small-2 logo columns"><a class="sub-link" href="index.html"><img src="img/logo.svg" alt="logo"></a></div>
+      <div class="large-10 medium-10 small-10 columns">
+        <h2>Your message has been sent!</h2>
+        <div class="large-12 medium-12 hide-for-small-only columns"><hr></div>
+      </div>
+    </div>
+
+    <div class="row show-for-small-only">
+      <div class="large-12 medium-12 columns small-hr"><hr></div>
+    </div>
+
+</div>
+	
+	<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script>
+	<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+
+</body>
+
+</html>
